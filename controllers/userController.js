595d36bce4f9f247a8f447c1d2f358d737ea0004
Jason897571 <king.gs1314@outlook.com>
2024-03-28 16:47:00 -0400

    complete User methods and working on friends

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const { User, Thought } = require('../models');
 
 module.exports = {
+    // get information for all users
     async getAllUsers(req,res) {
         try{
             const userData = await User.find({}).select('-__v');
@@ -10,17 +11,20 @@ module.exports = {
         }
 
     },
-
+    // get information for one user
     async getSingleUser(req,res) {
         try{
             // get thought and friend data
             const userData = await User.findOne({_id:req.params.userId}).select('-__v');
+            if(!userData){
+                return res.status(404).json({message: 'No user with that ID'})
+            }
             res.json(userData);
         }catch(err){
             res.status(500).json(err);
         }
     },
-
+    // create a new user
     async createNewUser(req,res){
         try{
             const userData = await User.create(req.body);
@@ -30,15 +34,40 @@ module.exports = {
             res.status(500).json(err);
         }
     },
-
+    //update the information for a user
     async updateUser(req,res){
         try{
-            const userData = await User.findOneAndUpdate({_id:req.params.userId},{$set:{"username":req.body.username,"email":req.body.email}})
+            const userData = await User.findOneAndUpdate({_id:req.params.userId},{$set:{"username":req.body.username,"email":req.body.email}},{new:true})
             res.json(userData);
         }
         catch(err){
             res.status(500).json(err);
         }
+    },
+
+    // delete a user 
+    async deleteUser(req,res){
+        try{
+            const userData = await User.findOneAndDelete({_id:req.params.userId});
+            if (userData == null) {
+                return res.status(404).json({ message: 'User not found' });
+              }
+            res.status(200).json({message:"user has been deleted","deleted user": userData});
+        }catch(err){
+            res.status(500).json(err);
+        }
+    },
+
+    //add a friend
+    async addFriend(req,res){
+        try{
+            const friendData = await User.findOneAndUpdate({_id:req.params.userId},{$addToSet:{"friends":req.friendData}},{new:true})
+            res.json(friendData);
+        }
+        catch(err){
+            res.status(500).json(err);
+        }
     }
+    
 
 }
--- models/User.js
+++ models/User.js
@@ -32,6 +32,8 @@ const userSchema = new Schema(
     }
 )
 
+
+
 userSchema.virtual('friendCount').get(function() {
     return this.friends.length;
 })
